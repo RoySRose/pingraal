10e89b44623bf3505431f9947ecac7f6cdc03fd3
Roy Kim <sungwook0115.kim@gmail.com>
2019-12-02 20:07:24 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+
+
+Testing Graalvm Truffle instrumentation APIs
+
+Goal is to create Pinpoint Graalvm Agent to monitor applications running on Graalvm
+
+ ## Reference
+ 
+ https://github.com/graalvm/simpletool 
---
+++ helloworld.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var num = 8083
+
+http.createServer(function (request, response) {
+    // Send the HTTP header
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // Send the response body as "Hello World"
+    response.end(func1() + 'Hello World\n');
+}).listen(num);
+
+function func1() {
+    return 2;
+}
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:' + num + '/');
